568c2d0c0eb56e1abd8de0b21ec8e658a0ff315e
jeyjey001 <160228166+jeyjey001@users.noreply.github.com>
2024-05-30 22:07:33 +0300

    script.js

--- project tee/script.js
+++ project tee/script.js
@@ -1,25 +1,15 @@
-// Add an event listener to the window object to detect when the window is resized
-window.addEventListener('resize', function() {
-    // Call the responsive function when the window is resized
-    makeResponsive();
-});
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-// Function to make the site responsive
-function makeResponsive() {
-    // Get the current width of the window
-    var screenWidth = window.innerWidth;
-
-    // Check if the screen width is below 799px (for mobile and tablet devices)
-    if (screenWidth < 800) {
-        // Hide the desktop navigation and show the mobile navigation
-        document.getElementById('navbar').style.display = 'none';
-        document.getElementById('mobile').style.display = 'flex';
-    } else {
-        // Show the desktop navigation and hide the mobile navigation
-        document.getElementById('navbar').style.display = 'flex';
-        document.getElementById('mobile').style.display = 'none';
-    }
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
 }
 
-// Call the makeResponsive function initially to set up the site for the current screen size
-makeResponsive();
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
